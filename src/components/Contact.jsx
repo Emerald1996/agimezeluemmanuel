ce3c77ba74cefbc973f5e37ee601885f48b595c6
Agimezelu Emmanuel <87588776+Emerald1996@users.noreply.github.com>
2022-12-26 06:36:53 +0100

    Update Contact.jsx
    Added height

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
   }
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/7a1ed756-ffdf-4979-95a2-184c33e51429" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/7a1ed756-ffdf-4979-95a2-184c33e51429" className='flex flex-col max-w-[600px] w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 mt-4 py-4'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - agimezeluemmanuel@gmail.com</p>
