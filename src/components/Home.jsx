7c7955d873fa45badde1713208cb45a026b7a7a7
Emmanuel Emerald Agimezelu <87588776+Emerald1996@users.noreply.github.com>
2024-06-03 13:14:50 +0100

    Updated Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,15 +9,15 @@ const Home = () => {
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
-          Agimezelu Emmanuel
+          Emmanuel Emerald A.
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
           I'm a web Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a junior web developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive frontend web applications.
+          I’m a customer-focused web developer specializing in building (and occasionally designing) exceptional digital ex
+          periences. With a background in customer support, I bring a unique perspective to crafting responsive frontend web
+          applications.
         </p>
         <div>
           <Link to='work' smooth={true} duration={500}>
