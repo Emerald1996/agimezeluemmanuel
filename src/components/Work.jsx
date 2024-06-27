e5ab865e07c742efe4072f511b9bc83018080e88
Emmanuel Emerald Agimezelu <87588776+Emerald1996@users.noreply.github.com>
2024-06-27 22:51:47 +0100

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import carWebsite from '../assets/cars-project.png'
 import ecommerce from '../assets/e-commerce-project.png'
 import natours from '../assets/natours.png'
 import countries from '../assets/country-data-project.png'
-import ship from '../assets/sailingBoat.png'
+import car from '../assets/car-project.png'
 
 const Work = () => {
   return (
@@ -143,16 +143,16 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${ship})`, backgroundSize: 'contain'  }}
+            style={{ backgroundImage: `url(${car})`, backgroundSize: 'contain'  }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                Ship Animation
+                Car Animation
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://sailing-boat.vercel.app/' target="_blank"rel='noreferrer'>
+                <a href='http://moving-car-eta.vercel.app/' target="_blank"rel='noreferrer'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
