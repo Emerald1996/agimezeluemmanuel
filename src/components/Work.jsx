97b67c49ec61b00d991f7d14f397c919238c3757
Emmanuel Emerald Agimezelu <87588776+Emerald1996@users.noreply.github.com>
2024-06-27 09:41:30 +0100

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import youtubeClone from '../assets/youtube-clone.png'
-import carWebsite from '../assets/carWebsite.png'
+import carWebsite from '../assets/cars-project.png'
 import ecommerce from '../assets/e-commerce-project.png'
-import linkedin from '../assets/linkedin.png'
+import natours from '../assets/natours.png'
 import calculator from '../assets/calculator.png'
 import ship from '../assets/sailingBoat.png'
 
@@ -45,16 +45,16 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${linkedin})` }}
+            style={{ backgroundImage: `url(${natours})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                LlinkedIn clone
+                Tour website
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://linkedin-clone-6ca61.firebaseapp.com' target="_blank"rel='noreferrer'>
+                <a href='https://emerald-tours.netlify.app/' target="_blank"rel='noreferrer'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
