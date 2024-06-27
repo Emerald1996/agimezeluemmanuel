a028b3d683ef31bd9cc268905930a1813a4ddc0b
Emmanuel Emerald Agimezelu <87588776+Emerald1996@users.noreply.github.com>
2024-06-27 10:12:36 +0100

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,7 +3,7 @@ import youtubeClone from '../assets/youtube-clone.png'
 import carWebsite from '../assets/cars-project.png'
 import ecommerce from '../assets/e-commerce-project.png'
 import natours from '../assets/natours.png'
-import calculator from '../assets/calculator.png'
+import countries from '../assets/country-data-project.png'
 import ship from '../assets/sailingBoat.png'
 
 const Work = () => {
@@ -120,16 +120,16 @@ const Work = () => {
           
             {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${calculator})` }}
+            style={{ backgroundImage: `url(${countries})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-               Functioning Calculator
+               Country-data
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://basic-calculator-xi.vercel.app/' target="_blank"rel='noreferrer'>
+                <a href='https://country-data-lemon.vercel.app/' target="_blank"rel='noreferrer'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
